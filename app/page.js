b8cf60d555459fd12f2e6ca5bd22fa0ac973c3de
Dimitar Mitev <d.mitev920123@gmail.com>
2024-10-12 21:02:34 +0300

    feat: heading and roomcard components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/Room.Card";
+import Heading from "@/components/Heading";
 export default function Home() {
   return (
     <>
-      <h1>Bookin App</h1>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard key={room.id} room={room} />)
+      ) : (
+        <p>No rooms available</p>
+      )}
     </>
   );
 }
